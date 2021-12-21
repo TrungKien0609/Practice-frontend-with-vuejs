ff5141ef0908bf14f7c8d201954e9dcdf4524573
Trung Kien <trungkien7300@gmail.com>
2021-12-21 17:19:26 +0700

    first commit for TodoList

---
+++ todo-list/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+   
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  computed: {
+   
+  },
+  methods: {
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
